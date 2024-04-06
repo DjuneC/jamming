9f7d7cb209a719c6d885ae51536803a1e38227a7
Rodelin J Cesar <cesarrodelin@outlook.com>
2024-04-06 14:14:46 -0500

    Use of hook to handle input change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import './App.css'
 
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+
+import songs from './data/data';
 
 function App() {
+  const [queryInput, setQueryInput] = useState('')
+
+  const handleQueryChange = (e) => {
+    const query = e.target.value;
+    setQueryInput(query)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(queryInput);
+    alert('test is good!')
+    setQueryInput('')
+  }
 
   return (
     <>
-      <Home />
+      <Home 
+      handleSubmit={handleSubmit}
+      handleChange={handleQueryChange} 
+      queryValue={queryInput} 
+      data={songs}
+      />
     </>
   )
 }
