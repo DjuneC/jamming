da0ea932cec23527ff39372898537b8666e8c3ea
Rodelin J Cesar <cesarrodelin@outlook.com>
2024-04-06 14:19:35 -0500

    Add some dummy data to test our logic without an API

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+const songs = [
+    {
+      songName: "Uptown Funk",
+      artistName: "Mark Ronson ft. Bruno Mars",
+      album: "Uptown Special"
+    },
+    {
+      songName: "Bohemian Rhapsody",
+      artistName: "Queen",
+      album: "A Night at the Opera"
+    },
+    {
+      songName: "Someone You Loved",
+      artistName: "Lewis Capaldi",
+      album: "Divinely Uninspired To A Hellish Extent"
+    },
+    {
+      songName: "A Sky Full of Stars",
+      artistName: "Coldplay",
+      album: "Ghost Stories"
+    },
+    {
+      songName: "What a Wonderful World",
+      artistName: "Louis Armstrong",
+      album: "The Complete Louis Armstrong Columbia Recordings (1928-1931)"
+    },
+    {
+      songName: "Billie Jean",
+      artistName: "Michael Jackson",
+      album: "Thriller"
+    },
+    {
+      songName: "Imagine",
+      artistName: "John Lennon",
+      album: "Imagine"
+    },
+    {
+      songName: "Hotel California",
+      artistName: "Eagles",
+      album: "Hotel California"
+    },
+    {
+      songName: "Lose Yourself",
+      artistName: "Eminem",
+      album: "8 Mile"
+    },
+    {
+      songName: "Rolling in the Deep",
+      artistName: "Adele",
+      album: "21"
+    }
+  ];
+  
+export default songs;
