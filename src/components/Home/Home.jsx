d6f6eb287d5b24e88a652c56170eea968b7346f2
Rodelin J Cesar <cesarrodelin@outlook.com>
2024-04-06 14:16:34 -0500

    Add Tracklist component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import NavBar from "../NavBar/NavBar"
+import Searchbar from "../SearchBar/Searchbar"
+import Tracklist from "../Tracklist/Tracklist"
 
-
-const Home = () => {
+const Home = ({handleSubmit, handleChange, queryValue, data}) => {
   return (
     <>
       <NavBar />
+      <Searchbar handleSubmit={handleSubmit} handleChange={handleChange} queryValue={queryValue} />
+      <Tracklist songs={data}/>
     </>
   )
 }
